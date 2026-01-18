ffb5b0dc969ce184e6082232538b5d4019c672d3
dattebayo <fkgame061@gmail.com>
2026-01-18 08:24:06 -0800

    not much changed

--- mux.go
+++ mux.go
@@ -6,14 +6,15 @@ func Mux() *http.ServeMux {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./frontend"))
 
+	//http handles
 	mux.Handle("/", fs)
 	mux.HandleFunc("GET /check", handleCheck)
 	mux.HandleFunc("POST /api/start-ftp", handleStart)
 	mux.HandleFunc("POST /api/stop-ftp", handleStop)
 	mux.HandleFunc("GET /api/logs", handleLogs)
 	//FTP handles
-	mux.HandleFunc("GET /api/list", handleLS)
-	mux.HandleFunc("GET /api/download", handleFile)
+	mux.HandleFunc("GET /api/ls", handleLS)
+	mux.HandleFunc("GET /api/download-file", handleDownloadFile)
 	mux.HandleFunc("POST /api/client/auth", handleAuthClient)
 	return mux
 }
