01a1407f6cab9a6d1863b5c4b6e072479d5ae828
dattebayo <fkgame061@gmail.com>
2026-01-23 09:26:58 -0800

    fixed UI freeze and Form not being posted

--- mux.go
+++ mux.go
@@ -13,21 +13,21 @@ func Mux() *http.ServeMux {
 	fs := http.FileServer(http.Dir("./frontend"))
 	//http Handles
 	mux.Handle("/", fs)                                      //serve frontend
-	mux.HandleFunc("GET /api/logs", httphandlers.HandleLogs) //get logs
+	mux.HandleFunc("/api/logs", httphandlers.HandleLogs) //get logs
 	//FTP server Handles
 	//required Form variables: server_port, server_root_dir, anonymous_allowed, write_allowed
-	mux.HandleFunc("POST /api/start-ftp", serverhandlers.HandleStartFTP) //start ftp server
-	mux.HandleFunc("POST /api/stop-ftp", serverhandlers.HandleStopFTP)   //stop ftp server	// TODO: change path to api/ftp/server/
-	//returns string responses, "running" and "not-running"
-	mux.HandleFunc("GET /api/ftp/server/status", serverhandlers.HandleServerStatus)
+	mux.HandleFunc("/api/start-ftp", serverhandlers.HandleStartFTP) //start ftp server
+	mux.HandleFunc("/api/stop-ftp", serverhandlers.HandleStopFTP)   //stop ftp server	// TODO: change path to api/ftp/server/
+	//returns string responses: "running" and "not-running"
+	mux.HandleFunc("/api/ftp/server/status", serverhandlers.HandleServerStatus)
 	//required query variables: path, returns the folder's entries in a list of FSObject, see ftp/config/config.go for FSObject
-	mux.HandleFunc("GET /api/ftp/server/ls", serverhandlers.HandleListDir) //list other server directory
+	mux.HandleFunc("/api/ftp/server/ls", serverhandlers.HandleListDir) //list other server directory
 	//required query variables: path, returns the file at path
-	mux.HandleFunc("GET /api/ftp/server/get-file", serverhandlers.HandleServeFile) //get file from other server
+	mux.HandleFunc("/api/ftp/server/get-file", serverhandlers.HandleServeFile) //get file from other server
 	//nothing required, returns a list of addresses of connected clients
-	mux.HandleFunc("GET /api/ftp/server/connected-clients", serverhandlers.HandleGetConnectedClients) //get clients connected to own server
+	mux.HandleFunc("/api/ftp/server/connected-clients", serverhandlers.HandleGetConnectedClients) //get clients connected to own server
 	//FTP client Handles
 	//required Form variables: server_host, server_port, user, password, anonymous
-	mux.HandleFunc("POST /api/ftp/client/connect-to-server", clienthandlers.HandleConnectToServer) //connect to other server
+	mux.HandleFunc("/api/ftp/client/connect-to-server", clienthandlers.HandleConnectToServer) //connect to other server
 	return mux
 }
