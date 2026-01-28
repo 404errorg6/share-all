091f094ca57b12ad93e5df6ed5cd263e7becc158
dattebayo <fkgame061@gmail.com>
2026-01-28 19:45:28 +0500

    still trynna fix upload

--- ftp/server/main_driver.go
+++ ftp/server/main_driver.go
@@ -21,6 +21,7 @@ func (d *AndroidMainDriver) GetSettings() (*ftpserver.Settings, error) {
 		PublicHost:             config.Server.FTPHost,
 		ActiveConnectionsCheck: ftpserver.IPMatchRequired,
 		PasvConnectionsCheck:   ftpserver.IPMatchRequired,
+		IdleTimeout:            300,
 	}
 	return &settings, nil
 }
