eabc88ed5c5c53aa4722511f7c7dd1ec42b9da3a
dattebayo <fkgame061@gmail.com>
2026-03-04 21:33:39 +0500

    found some bugs

--- handlers/http_handlers/start_web_ui.go
+++ handlers/http_handlers/start_web_ui.go
@@ -1,6 +1,8 @@
 package httphandlers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/404errorg6/FTP-server/ftp/config"
@@ -11,12 +13,16 @@ var (
 )
 
 func HandleStartWebUI(w http.ResponseWriter, req *http.Request) {
-	server := &http.Server{
-		Addr: addr,
-	}
+	/*	server := &http.Server{
+			Addr: addr,
+		}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		config.LogsCh <- err.Error()
-	}
+		err := server.ListenAndServe()
+		if err != nil {
+			config.LogsCh <- err.Error()
+		}
+	*/
+	fmt.Printf("Starting unsecure sharing...\n")
+
+	go func() { log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(config.DefLocalDir)))) }()
 }
