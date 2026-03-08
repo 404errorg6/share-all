13021c62bc6852d5c5d7ea8b343b5ae01fd855d2
dattebayo <fkgame061@gmail.com>
2026-03-08 12:49:20 +0500

    offline http server ready

--- handlers/http_handlers/start_web_ui.go
+++ handlers/http_handlers/start_web_ui.go
@@ -1,28 +1,57 @@
 package httphandlers
 
 import (
+	"embed"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/404errorg6/FTP-server/ftp/config"
+	clienthandlers "github.com/404errorg6/FTP-server/handlers/client_handlers"
 )
 
-var (
-	addr = "0.0.0.0:8080"
-)
+//go:embed share.*
+var assets embed.FS
 
 func HandleStartWebUI(w http.ResponseWriter, req *http.Request) {
-	/*	server := &http.Server{
-			Addr: addr,
-		}
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: miniMux(),
+	}
 
-		err := server.ListenAndServe()
-		if err != nil {
-			config.LogsCh <- err.Error()
-		}
-	*/
-	fmt.Printf("Starting unsecure sharing...\n")
+	config.LogsCh <- fmt.Sprintln("Starting unsecure sharing...")
 
-	go func() { log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(config.DefLocalDir)))) }()
+	err := server.ListenAndServe()
+	if err != nil {
+		config.LogsCh <- err.Error()
+	}
+}
+
+func miniMux() *http.ServeMux {
+	mux := &http.ServeMux{}
+
+	mux.HandleFunc("GET /", serveHTML)
+
+	mux.HandleFunc("GET /api/ls", clienthandlers.HandleListLocalDir)
+	mux.HandleFunc("GET /file", serveFile)
+
+	return mux
+}
+
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, "share.html", http.StatusPermanentRedirect)
+	}
+	httpServer := http.FileServer(http.FS(assets))
+	httpServer.ServeHTTP(w, r)
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	localPath := r.FormValue("local_path")
+	if localPath == "" {
+		http.Error(w, "local_path is required", http.StatusBadRequest)
+		return
+	}
+
+	localPath = config.ResolveLocalPath(localPath)
+	http.ServeFile(w, r, localPath)
 }
